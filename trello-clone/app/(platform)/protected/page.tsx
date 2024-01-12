dfbe4a946a980ce95d11889e9e1609c3bf70d5b8
Hasan Ali <hasan.ali@dazn.com>
2024-01-12 19:38:27 +0530

    Added authentication

--- trello-clone/app/(platform)/protected/page.tsx
+++ trello-clone/app/(platform)/protected/page.tsx
@@ -1,9 +1,13 @@
-import { currentUser } from '@clerk/nextjs';
+import { UserButton, currentUser } from '@clerk/nextjs';
 
 const Protected = async () => {
   const user = await currentUser();
-  return <div>{JSON.stringify(user, null, 4)}</div>;
+  return (
+    <div>
+      {JSON.stringify(user, null, 4)}
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 };
 
 export default Protected;
-    
