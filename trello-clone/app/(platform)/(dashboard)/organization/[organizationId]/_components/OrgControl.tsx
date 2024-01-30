5e07bf505bb99c4dc1118af72f4dba51eaa1f433
Hasan Ali <hasan.ali@dazn.com>
2024-01-30 18:14:42 +0530

    Added layouts, sidebar and some more UI

---
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/_components/OrgControl.tsx
@@ -0,0 +1,18 @@
+import { useOrganizationList } from '@clerk/nextjs';
+import { useParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+export const OrgControl = () => {
+  const { organizationId } = useParams();
+  const { setActive } = useOrganizationList();
+
+  useEffect(() => {
+    if (!setActive) return;
+
+    setActive({
+      organization: organizationId as string,
+    });
+  }, [setActive, organizationId]);
+
+  return null;
+};
---
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,5 @@
+function OrganizationIdPage() {
+  return <div className='organization-id'>Organization Id page</div>;
+}
+
+export default OrganizationIdPage;
